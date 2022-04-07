12c08f34ed4455a00d65cfdcaca4cf8ff64dcb9a
VovikZip <alioshkin75@gmail.com>
2022-04-07 15:17:20 +0300

    flex page

--- script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function showFunction() {
-    let x = document.getElementById('myTopnav');
-
-    if (x.className === "topnav"){
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
